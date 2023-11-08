3dfbf5480612466ad21a9838b880776d3631207c
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-08 12:53:39 -0500

    ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Clara
+ * Created on: Nov 8th
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Silly)
+
+// servo turns 180 degrees
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+
+  // show amount turned
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallSquare)
+
+  // clear screen 
+  basic.clearScreen()
+  basic.showIcon(IconNames.Silly)
+})
+
+// servo turns to 0 degrees
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  // show amount turned 
+  basic.showString('0')
+  basic.showIcon(IconNames.SmallSquare)
+  // clear screen
+  basic.clearScreen()
+  basic.showIcon(IconNames.Silly)
+})
